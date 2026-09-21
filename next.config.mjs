f533a0a81f3e2f418d981b9e271d0c103fd2c820
Gabriel <Drakuno385@gmail.com>
2026-09-21 09:19:35 -0300

    fix: ignore eslint during vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {};
+﻿/** @type {import("next").NextConfig} */
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },};
 export default nextConfig;
